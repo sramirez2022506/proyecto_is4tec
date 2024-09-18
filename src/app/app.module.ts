ac3b1cb13faaaa0a97a4f7559dc5808309c48522
sramirez-2022506 <sramirez-2022506@kinal.edu.gt>
2024-09-18 04:55:53 -0600

    Error al mostrar vista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import SharedModule from './shared/shared.module';
-/* import { AdminModule } from './admin/admin.module'; */
+import { AdminModule } from './admin/admin.module';
 import { AppRoutingModule } from './app-routing.module';
 
 import { HomeComponent } from './home/home.component';
